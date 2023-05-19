2aee68318a9f707afc976d76b1b03a83ee1bcb06
Morlay <morlay.null@gmail.com>
2023-05-19 11:21:14 +0800

    fix: annotation name part should not more than 63 chars

--- pkg/annotation/reload.go
+++ pkg/annotation/reload.go
@@ -9,10 +9,12 @@ const ReloadHash = "reload.octohelm.tech/hash"
 const ReloadSecret = "reload.octohelm.tech/secret"
 const ReloadConfigMap = "reload.octohelm.tech/configmap"
 
+// https://kubernetes.io/docs/concepts/overview/working-with-objects/annotations/#syntax-and-character-set
+// <prefix>/<name-part>
 func ConfigMapHashKey(name string) string {
-	return fmt.Sprintf("octohelm.tech/configmap.%s.hash", name)
+	return fmt.Sprintf("hash.configmap.octohelm.tech/%s", name)
 }
 
 func SecretHashKey(name string) string {
-	return fmt.Sprintf("octohelm.tech/secret.%s.hash", name)
+	return fmt.Sprintf("hash.secret.octohelm.tech/%s", name)
 }
